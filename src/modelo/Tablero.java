d73e35ce30fdad941113d91c6b4e3dbc11910f65
Santiago Mitidiero <santimiti10@gmail.com>
2025-05-14 15:32:10 -0300

    Agregar lógica para verificar adyacencias entre posiciones en el tablero

--- src/modelo/Tablero.java
+++ src/modelo/Tablero.java
@@ -1,9 +1,43 @@
 package modelo;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Tablero {
     private final Ficha[][] tablero;
     private static final int FILAS = 7;
     private static final int COLUMNAS = 7;
+    private static final Map<String, List<String>> adyacencias = new HashMap<>();
+
+    static {
+        // Definición de adyacencias
+        adyacencias.put("0,0", Arrays.asList("0,3", "3,0"));
+        adyacencias.put("0,3", Arrays.asList("0,0", "1,3", "0,6"));
+        adyacencias.put("0,6", Arrays.asList("0,3", "3,6"));
+        adyacencias.put("1,1", Arrays.asList("3,1", "1,3"));
+        adyacencias.put("1,3", Arrays.asList("1,1", "0,3", "1,5", "2,3"));
+        adyacencias.put("1,5", Arrays.asList("1,3", "3,5"));
+        adyacencias.put("2,2", Arrays.asList("3,2", "2,3"));
+        adyacencias.put("2,3", Arrays.asList("2,2", "1,3", "2,4"));
+        adyacencias.put("2,4", Arrays.asList("2,3", "3,4"));
+        adyacencias.put("3,0", Arrays.asList("0,0", "6,0", "3,1"));
+        adyacencias.put("3,1", Arrays.asList("3,0", "1,1", "3,2", "5,1"));
+        adyacencias.put("3,2", Arrays.asList("3,1", "4,2", "2,2"));
+        adyacencias.put("3,4", Arrays.asList("4,4", "2,4", "3,5"));
+        adyacencias.put("3,5", Arrays.asList("3,4", "5,5", "1,5", "3,6"));
+        adyacencias.put("3,6", Arrays.asList("0,6", "3,5", "6,6"));
+        adyacencias.put("4,2", Arrays.asList("4,3", "3,2"));
+        adyacencias.put("4,3", Arrays.asList("4,2", "4,4", "5,3"));
+        adyacencias.put("4,4", Arrays.asList("4,3", "3,4"));
+        adyacencias.put("5,1", Arrays.asList("3,1", "5,3"));
+        adyacencias.put("5,3", Arrays.asList("5,5", "5,1", "4,3", "6,3"));
+        adyacencias.put("5,5", Arrays.asList("5,3", "3,5"));
+        adyacencias.put("6,0", Arrays.asList("3,0", "6,3"));
+        adyacencias.put("6,3", Arrays.asList("6,0", "5,3", "6,6"));
+        adyacencias.put("6,6", Arrays.asList("3,6", "6,3"));
+    }
 
     public Tablero() {
         this.tablero = new Ficha[FILAS][COLUMNAS];
@@ -47,16 +81,19 @@ public class Tablero {
         if (posicionOcupada(filaDestino, columnaDestino)) {
             throw new IllegalStateException("La posición de destino ya está ocupada por otra ficha");
         }
+        if (!sonAdyacentes(filaOrigen, columnaOrigen, filaDestino, columnaDestino)) {
+            throw new IllegalStateException("Las posiciones no son adyacentes");
+        }
 
         Ficha ficha = tablero[filaOrigen][columnaOrigen];
         tablero[filaDestino][columnaDestino] = ficha;
         tablero[filaOrigen][columnaOrigen] = null;
-        ficha.moverFicha(filaDestino, columnaDestino);
     }
 
     public boolean sonAdyacentes(int fila1, int columna1, int fila2, int columna2) {
-        //TODO algoritmo para verificar si las posiciones son adyacentes en el tablero
-        return false;
+        String pos1 = fila1 + "," + columna1;
+        String pos2 = fila2 + "," + columna2;
+        return adyacencias.containsKey(pos1) && adyacencias.get(pos1).contains(pos2);
     }
 
     public void imprimirTablero() {
